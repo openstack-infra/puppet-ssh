1483920384f4771fa039eabeda93bffe69ff285a
Morgan Fainberg <morgan.fainberg@gmail.com>
2016-05-22 18:05:03 -0700

    Update puppet-ssh for client config managment
    Update puppet-ssh to support configuring and managing the
    /etc/ssh/ssh_config file with optimisations for openstack.org
    ephemeral hosts (do not store the host keys in the known_hosts
    files).
    
    As part of this work, support of the SSH server configuration
    management should occur in the "ssh::server" module; puppet-ssh now
    supports either using "ssh" or "ssh::server" for the transitional
    period.
    
    Future changes will look at broadening the configuration options
    and validation of the configuration options for the client config
    such as richer host matching, stricthostkeychecking with pre-defined
    keys distributed, etc.
    
    Change-Id: Ic2dcc00945ab1eed985eb32dcfae910f4363cacb

---
+++ manifests/client.pp
@@ -0,0 +1,23 @@
+# Class: ssh::client
+#
+# configure the ssh client
+#
+# NOTE: "host_pattern_store_known_hosts" should be a standard host pattern
+# understood by ssh_config, see man ssh_config(5) for more information
+class ssh::client(
+    $host_pattern_store_known_hosts = '*.openstack.org',
+    $ssh_client_loglevel = 'QUIET',
+) {
+    include ::ssh::params
+    package { $::ssh::params::client_package_name:
+        ensure => present,
+    }
+    file { '/etc/ssh/ssh_config':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('ssh/ssh_config.erb'),
+        replace => true,
+    }
+}
