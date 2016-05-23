e1fc50032a3d72261e7365bc79fdbb8013df87c7
Morgan Fainberg <morgan.fainberg@gmail.com>
2016-05-22 18:05:03 -0700

    Update puppet-ssh for client config managment
    Update puppet-ssh to support configuring and managing the
    /etc/ssh/ssh_config file with optimisations for openstack.org
    ephemeral hosts (do not store the host keys in the known_hosts
    files).
    
    As part of this work, support of the SSH server configuration
    management should occur in the "ssh::server" module; puppet-ssh now
    supports either using "ssh" or "ssh::server" for the transitional
    period.
    
    Future changes will look at broadening the configuration options
    and validation of the configuration options for the client config
    such as richer host matching, stricthostkeychecking with pre-defined
    keys distributed, etc.
    
    Change-Id: Ic2dcc00945ab1eed985eb32dcfae910f4363cacb

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,19 +5,22 @@
 class ssh::params {
   case $::osfamily {
     'RedHat': {
-      $package_name = 'openssh-server'
-      $service_name = 'sshd'
-      $sftp_path    = '/usr/libexec/openssh/sftp-server'
+      $package_name         = 'openssh-server'
+      $client_package_name  = 'openssh-clients'
+      $service_name         = 'sshd'
+      $sftp_path            = '/usr/libexec/openssh/sftp-server'
     }
     'Debian': {
-      $package_name = 'openssh-server'
-      $service_name = 'ssh'
-      $sftp_path    = '/usr/lib/openssh/sftp-server'
+      $package_name         = 'openssh-server'
+      $client_package_name  = 'openssh-client'
+      $service_name         = 'ssh'
+      $sftp_path            = '/usr/lib/openssh/sftp-server'
     }
     'Suse': {
-        $package_name = 'openssh'
-        $service_name = 'sshd'
-        $sftp_path    = '/usr/lib/ssh/sftp-server'
+      $package_name         = 'openssh'
+      $client_package_name  = 'openssh'
+      $service_name         = 'sshd'
+      $sftp_path            = '/usr/lib/ssh/sftp-server'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} The 'ssh' module only supports osfamily Debian, RedHat or SUSE (slaves only).")
