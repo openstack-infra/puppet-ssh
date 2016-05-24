4cef6ca875ec15ecf8a8b5ddf1f12f4cae241084
Morgan Fainberg <morgan.fainberg@gmail.com>
2016-05-24 16:09:49 -0700

    Include ssh::server in ssh
    Instead of having potentially diverging classes that are duplicates
    include the submodule ssh::server in the main ssh module. Eventually
    it should not be needed to contsan ssh::server in the main ssh module
    as everything should eventually move to using ssh::server directly.
    
    Change-Id: I307c022097dfe55b4d663c13c036474d364d7841

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,8 @@
 # == Class: ssh
 #
 class ssh ($trusted_ssh_source = 'puppetmaster.openstack.org') {
-    include ::ssh::params
-    package { $::ssh::params::package_name:
-      ensure => present,
-    }
-    if ($::in_chroot) {
-      notify { 'sshd in chroot':
-        message => 'sshd not refreshed, running in chroot',
-      }
-    } else {
-      service { $::ssh::params::service_name:
-        ensure     => running,
-        hasrestart => true,
-        subscribe  => File['/etc/ssh/sshd_config'],
-      }
-    }
-    file { '/etc/ssh/sshd_config':
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0444',
-      content => template('ssh/sshd_config.erb'),
-      replace => true,
+    include ::ssh::server
+    ::ssh::server {
+        trusted_ssh_source => $trusted_ssh_source,
     }
 }
